2f9d931c601db4abc6098fbc6feb9976e6d17c98
Sammwy <sammwy.dev@gmail.com>
2023-06-02 13:13:33 -0300

    feature: better error handling

--- src/errors/Exception.ts
+++ src/errors/Exception.ts
@@ -3,7 +3,7 @@ import { HTTPException } from 'hono/http-exception';
 export default class Exception extends HTTPException {
   constructor(status: number, kind: string, message: string) {
     super(status, {
-      res: new Response(JSON.stringify({ type: 'error', kind, message }), {
+      res: new Response(JSON.stringify({ error: { kind, message } }), {
         status,
         headers: {
           'Content-Type': 'application/json',
